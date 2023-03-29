b6585f88b8b411575afb2d693cdf124af71f7f5e
Esakkimuthu <esakkimuthu@Esakkimuthus-MacBook-Air.local>
2023-03-29 18:54:17 +0530

    wp table created

--- wp-theme-files-timestamp.php
+++ wp-theme-files-timestamp.php
@@ -9,4 +9,19 @@
  */
 
 
- 
+ function theme_file_tracker_install() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'theme_file_tracker';
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE $table_name (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        file_path varchar(255) NOT NULL,
+        modified_date datetime NOT NULL,
+        PRIMARY KEY  (id),
+        UNIQUE KEY file_path (file_path)
+    ) $charset_collate;";
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+    add_option( 'theme_file_tracker_db_version', '1.0' );
+}
+register_activation_hook( __FILE__, 'theme_file_tracker_install' );
