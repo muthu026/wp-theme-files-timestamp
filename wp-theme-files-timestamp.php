bed3e22befe6e7a6afec8ea101f9fecfa16eeb72
Esakkimuthu <esakkimuthu@Esakkimuthus-MacBook-Air.local>
2023-03-29 18:55:27 +0530

    latest files stores on db

--- wp-theme-files-timestamp.php
+++ wp-theme-files-timestamp.php
@@ -25,3 +25,31 @@
     add_option( 'theme_file_tracker_db_version', '1.0' );
 }
 register_activation_hook( __FILE__, 'theme_file_tracker_install' );
+
+
+function theme_file_tracker() {
+global $wpdb;
+$theme_dir = get_template_directory();
+$updated_files = array();
+$previous_dates = $wpdb->get_results("SELECT file_path, modified_date FROM {$wpdb->prefix}theme_file_tracker", ARRAY_A);
+
+
+foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($theme_dir)) as $file) {
+    if ($file->isFile()) {
+        $file_path = $file->getPathname();
+        $file_modified_date = $file->getMTime();
+        $previous_date = isset($previous_dates[$file_path]) ? strtotime($previous_dates[$file_path]) : 0;
+        if ($file_modified_date > $previous_date) {
+            $updated_files[] = $file_path;
+        }
+        $wpdb->replace("{$wpdb->prefix}theme_file_tracker", array('file_path' => $file_path, 'modified_date' => date('Y-m-d H:i:s', $file_modified_date)), array('%s', '%s'));
+    }
+    
+}
+
+if (!empty($updated_files)) {
+    // Send an email notification or display a message on the dashboard
+}
+
+}
+add_action('init', 'theme_file_tracker');
